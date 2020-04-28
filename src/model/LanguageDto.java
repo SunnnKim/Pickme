e565ab3d840e2d438326ec5e006f7debf4b6d0e1
javadv2020 <javadv2020@gmail.com>
2020-04-28 16:33:01 +0900

    김은청 : 이력서 작성 완료

---
+++ src/model/LanguageDto.java
@@ -0,0 +1,101 @@
+package model;
+/*
+CREATE TABLE LANGUAGE
+(
+    `RSMSEQ`  INT              NULL        COMMENT '이력서 시퀀스', 
+    `LANG`    VARCHAR(1000)    NULL        COMMENT '언어', 
+    `LEVEL`   VARCHAR(1000)    NULL        COMMENT '수준', 
+    `TEST`    VARCHAR(1000)    NULL        COMMENT '시험명', 
+    `SCORE`   VARCHAR(1000)    NULL        COMMENT '점수/급', 
+    `DATE`    VARCHAR(100)     NULL        COMMENT '시험날짜'
+);
+
+ALTER TABLE LANGUAGE COMMENT '이력서 외국어 테이블';
+*/
+public class LanguageDto {
+	
+	int rsmseq;		// 이력서 시퀀스
+	String lang; 	// 언어
+	String level;	// 수준
+	String test; 	// 시험명
+	String score;	// 점수/급
+	String date;	// 시험날짜
+	
+	
+	
+	public LanguageDto() {
+
+	}
+	
+	public LanguageDto(int rsmseq, String lang, String level, String test, String score, String date) {
+		super();
+		this.rsmseq = rsmseq;
+		this.lang = lang;
+		this.level = level;
+		this.test = test;
+		this.score = score;
+		this.date = date;
+	}
+
+	
+	public int getRsmseq() {
+		return rsmseq;
+	}
+
+	public void setRsmseq(int rsmseq) {
+		this.rsmseq = rsmseq;
+	}
+	
+	public String getLang() {
+		return lang;
+	}
+	
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+	
+	public String getLevel() {
+		return level;
+	}
+	
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	
+	public String getTest() {
+		return test;
+	}
+	
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+	public String getScore() {
+		return score;
+	}
+	
+	public void setScore(String score) {
+		this.score = score;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	@Override
+	public String toString() {
+		return "LanguageDto [rsmseq=" + rsmseq + ", lang=" + lang + ", level=" + level + ", test=" + test + ", score="
+				+ score + ", date=" + date + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
--- src/pickme/com/a/resume/dao/ResumeDaoImpl.java
+++ src/pickme/com/a/resume/dao/ResumeDaoImpl.java
@@ -1,6 +1,78 @@
 package pickme.com.a.resume.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import model.AwardsEtcDto;
+import model.CareerDto;
+import model.EducationDto;
+import model.LanguageDto;
+import model.LinkDto;
+import model.ResumeDto;
+
+@Repository
 public class ResumeDaoImpl implements ResumeDao{
+	
+	@Autowired
+	SqlSession sqlSession;
+	String namespace = "Resume.";
+	
+	// 이력서 시퀀스 + 1
+	@Override
+	public int resumeGetSeq() {
+		return sqlSession.selectOne(namespace + "resumeGetSeq");
+	}
+		
+	// 이력서 기본 정보  insert 
+	@Override
+	public int resumeInsert(ResumeDto dto) {
+		int count = sqlSession.insert(namespace + "resumeInsert", dto);
+		return count;
+	}
+	
+	
+	// 경력  insert 
+	@Override
+	public int careerInsert(CareerDto dto) {
+		int count = sqlSession.insert(namespace + "careerInsert", dto);
+		return count;
+	}
+	
+	
+	// 학력 insert
+	@Override
+	public int educationInsert(EducationDto dto) {
+		int count = sqlSession.insert(namespace + "educationInsert", dto);
+		return count;
+	}
+	
+	// 수상 및 기타 insert
+	@Override
+	public int AwardsEtcInsert(AwardsEtcDto dto) {
+		int count = sqlSession.insert(namespace + "AwardsEtcInsert", dto);
+		return count;
+	}
+	
+	// 외국어 insert
+	@Override
+	public int LanguageInsert(LanguageDto dto) {
+		int count = sqlSession.insert(namespace + "LanguageInsert", dto);
+		return count;
+	}
+	
+	// 링크 insert
+	@Override
+	public int LinkInsert(LinkDto dto) {
+		int count = sqlSession.insert(namespace + "LinkInsert", dto);
+		return count;
+	}
+	
+
+	
+	
+	
+	
+	
 
 }
